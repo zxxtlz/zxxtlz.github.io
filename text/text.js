bcc8ae317147c3707e0b89e029ab3b58910a6405
zxxtlz <75642081+zxxtlz@users.noreply.github.com>
2023-02-27 19:55:45 +0200

    UPDATED
    haha

---
+++ text/text.js
@@ -0,0 +1,26 @@
+let visiblexd = false;
+document.getElementById("TextID")
+document.getElementById("Counting")
+document.getElementById("CanvasXD")
+
+const tekst = document.getElementById("TextID")
+const Buttonn  = document.getElementById("ButtonXD")
+
+Buttonn.onclick = function showhide(){
+    if (visiblexd){
+        tekst.style.visibility="hidden"
+        Buttonn.textContent="Show"
+        Buttonn.style.backgroundColor="red"
+
+        visiblexd=false
+    }
+    else{
+        tekst.style.visibility="visible"
+        Buttonn.textContent="Hide"
+        Buttonn.style.backgroundColor="blue"
+
+        visiblexd=true
+    }
+    Counting.textContent ++;
+}
+
