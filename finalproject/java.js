b6478e120c1335f18e7a127cfb4fc326964cf85f
zxxtlz <75642081+zxxtlz@users.noreply.github.com>
2023-04-01 12:24:59 +0300

    Updated website (still unfinished)
    haha

--- finalproject/java.js
+++ finalproject/java.js
@@ -42,5 +42,38 @@ function showSlides(n) {
       dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
   }
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+let Pricee = document.getElementById("Price")
+let Pricer = document.getElementById("amount")
+
+Pricer.onchange = function onChange() {
+  let x = Pricer.value
+  if (x == "1") {
+    Pricee.innerHTML = "Цена: 19.50лв"
+  }
+  if (x == "3") {
+    Pricee.innerHTML = "Цена: 55лв"
+  }
+  if (x == "5") {
+    Pricee.innerHTML = "Цена: 85лв"
+  }
+}
